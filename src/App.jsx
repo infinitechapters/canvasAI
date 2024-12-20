86bef1492040ebbeab2462138bd078e3f1a14bc3
A-R-P-I-T-JAIN <116783129+A-R-P-I-T-JAIN@users.noreply.github.com>
2024-12-20 19:55:08 +0530

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './Pages/Home'
+import "./App.css";
+import Home from "./Pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PlayGround from "./Pages/PlayGround";
 
 function App() {
-
   return (
     <>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/playground" element={<PlayGround />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
